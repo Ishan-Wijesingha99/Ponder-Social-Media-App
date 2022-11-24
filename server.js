8a28e138fb89fc3c08aadaa8436a623fa3f1fe15
Ishan <ishanwijes@gmail.com>
2022-11-24 23:25:11 +1100

    added mongoose connection

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const { ApolloServer } = require('apollo-server')
 const gql = require('graphql-tag') // installed with apollo-server npm package
 // there is no need to install express because it comes installed with apollo-server npm package
 
+const mongoose = require('mongoose')
+
+
 
 const typeDefs = gql`
   type Query {
@@ -13,16 +16,18 @@ const typeDefs = gql`
 
 const resolvers = {
   Query: {
-    sayHi: () => {
-      console.log('Hello there')
-    }
+    sayHi: () => 'Hi There'
   }
 }
 
 
 const server = new ApolloServer({ typeDefs, resolvers })
 
+mongoose.connect('mongodb+srv://ishanwij99:yeet981@cluster0.6n9gpsy.mongodb.net/Ponder?retryWrites=true&w=majority',
+{ useNewUrlParser: true })
+.then(res => console.log('Mongoose connection successful'))
+.then(() => server.listen({ port: 5000 }).then(res => console.log(`Server is running at ${res.url}`)))
 
-server.listen({ port: 5000 })
-.then(res => console.log(`Server is running at ${res.url}`))
+
+
 
